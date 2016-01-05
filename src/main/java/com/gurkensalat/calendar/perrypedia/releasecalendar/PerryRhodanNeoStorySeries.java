3f69515f0fa268899dc187687bf116eabcd00aca
Hakan Tandogan <hakan@tandogan.com>
2016-01-05 19:19:58 +0100

    'Calculate' releases dates of Neo Stories by lookup table, including unit tests

--- src/main/java/com/gurkensalat/calendar/perrypedia/releasecalendar/PerryRhodanNeoStorySeries.java
+++ src/main/java/com/gurkensalat/calendar/perrypedia/releasecalendar/PerryRhodanNeoStorySeries.java
@@ -1,14 +1,33 @@
 package com.gurkensalat.calendar.perrypedia.releasecalendar;
 
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PerryRhodanNeoStorySeries extends AbstractSeries
 {
-    // PRNS 1 - Oktober 2014 ???
-    public static final DateTime RELEASE_OF_ISSUE_1 = null;
+    private static final Logger logger = LoggerFactory.getLogger(AbstractSeries.class);
 
-    // PRNS 5 - November 2015 ???
-    public static final DateTime RELEASE_OF_ISSUE_5 = null;
+    // PRNS 1 - Oktober 2014 ???
+    public static final DateTime RELEASE_OF_ISSUE_1 = new DateTime().withDate(2014, 10, 1).withMillisOfDay(0);
+
+    // PRNS 2 - 22. Dezember 2014
+    public static final DateTime RELEASE_OF_ISSUE_2 = new DateTime().withDate(2014, 10, 1).withMillisOfDay(0);
+
+    // PRNS 3 - 23. März 2015
+    public static final DateTime RELEASE_OF_ISSUE_3 = new DateTime().withDate(2014, 10, 1).withMillisOfDay(0);
+
+    // PRNS 4 - Mai 2015
+    public static final DateTime RELEASE_OF_ISSUE_4 = new DateTime().withDate(2014, 10, 1).withMillisOfDay(0);
+
+    // PRNS 5 - Oktober 2015
+    public static final DateTime RELEASE_OF_ISSUE_5 = new DateTime().withDate(2014, 10, 1).withMillisOfDay(0);
+
+    // PRNS 6 - November 2015
+    public static final DateTime RELEASE_OF_ISSUE_6 = new DateTime().withDate(2014, 10, 1).withMillisOfDay(0);
+
+    // PRNS 7 - März 2015
+    public static final DateTime RELEASE_OF_ISSUE_7 = new DateTime().withDate(2014, 10, 1).withMillisOfDay(0);
 
     /**
      * {@inheritDoc}
@@ -21,20 +40,59 @@ public class PerryRhodanNeoStorySeries extends AbstractSeries
     /**
      * {@inheritDoc}
      */
-    public int getMaxIssue()
+    public int getMaxIssues()
     {
         return 7;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public DateTime getIssueReleaseDate(int issue)
+    {
+        logger.debug("Asked for release date of {}", issue);
+        DateTime result = null;
+
+        if (issue == 1)
+        {
+            result = RELEASE_OF_ISSUE_1;
+        }
+        else if (issue == 2)
+        {
+            result = RELEASE_OF_ISSUE_2;
+        }
+        else if (issue == 3)
+        {
+            result = RELEASE_OF_ISSUE_3;
+        }
+        else if (issue == 4)
+        {
+            result = RELEASE_OF_ISSUE_4;
+        }
+        else if (issue == 5)
+        {
+            result = RELEASE_OF_ISSUE_5;
+        }
+        else if (issue == 6)
+        {
+            result = RELEASE_OF_ISSUE_6;
+        }
+        else if (issue == 7)
+        {
+            result = RELEASE_OF_ISSUE_7;
+        }
+
+        return result;
+    }
+
     public DateTime getFirstIssueReleaseDate()
     {
-        // FIXME mock for now, implement properly...
-        return RELEASE_OF_ISSUE_1;
+        return getIssueReleaseDate(1);
     }
 
     public DateTime getFixedReleaseDate()
     {
-        return RELEASE_OF_ISSUE_5;
+        return getIssueReleaseDate(5);
     }
 
     public int getFixedReleaseIssue()
---
+++ src/test/java/com/gurkensalat/calendar/perrypedia/releasecalendar/PerryRhodanNeoStorySeriesTest.java
@@ -0,0 +1,48 @@
+package com.gurkensalat.calendar.perrypedia.releasecalendar;
+
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class PerryRhodanNeoStorySeriesTest
+{
+    private Series testable;
+
+    @Before
+    public void setUp()
+    {
+        testable = new PerryRhodanNeoStorySeries();
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void testFirstIssueReleaseDate()
+    {
+        // PRNS 1
+        // Calculated: 1. 10. 2014
+        // Perrypedia: Oktober 2014
+        DateTime expected = new DateTime().withDate(2014, 10, 1).withMillisOfDay(0);
+        DateTime actual = testable.getFirstIssueReleaseDate();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIssue50ReleaseDate()
+    {
+        // PRNS 50
+        // Calculated: null, too big to release
+        // Perrypedia: too far in the future...
+        DateTime expected = null;
+        DateTime actual = testable.getIssueReleaseDate(50);
+        assertNull(actual);
+        assertEquals(expected, actual);
+    }
+}
