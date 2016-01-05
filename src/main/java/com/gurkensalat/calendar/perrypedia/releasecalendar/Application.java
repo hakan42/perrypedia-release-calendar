abe40bebdf1551284f174a5ca7b3935f99140fcc
Hakan Tandogan <hakan@tandogan.com>
2016-01-05 15:32:59 +0100

    Set up a spring-boot command line runner Application

--- src/main/java/com/gurkensalat/calendar/perrypedia/releasecalendar/Application.java
+++ src/main/java/com/gurkensalat/calendar/perrypedia/releasecalendar/Application.java
@@ -1,9 +1,32 @@
 package com.gurkensalat.calendar.perrypedia.releasecalendar;
 
-public class Application
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner
 {
-    public static void main(String args[])
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private Environment environment;
+
+    public static void main(String[] args)
     {
+        SpringApplication.run(Application.class, args);
+    }
+
+    public void run(String... strings) throws Exception
+    {
+        logger.info("spring-boot command line runner main...");
+        logger.info(environment.getProperty("foo"));
+        logger.info("spring-boot command line runner main end...");
     }
 }
-
