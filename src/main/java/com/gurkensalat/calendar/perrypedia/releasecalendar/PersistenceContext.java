a409625278e2321c21ea73a7f69579f6fe546ecc
Hakan Tandogan <hakan@tandogan.com>
2016-01-07 09:12:58 +0100

    configuration of database connection

---
+++ src/main/java/com/gurkensalat/calendar/perrypedia/releasecalendar/PersistenceContext.java
@@ -0,0 +1,49 @@
+package com.gurkensalat.calendar.perrypedia.releasecalendar;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceContext
+{
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.connectionTestQuery}")
+    private String connectionTestQuery;
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource()
+    {
+        Properties dsProps = new Properties();
+        dsProps.setProperty("url", dataSourceUrl);
+        dsProps.setProperty("user", user);
+        dsProps.setProperty("password", password);
+
+        Properties configProps = new Properties();
+        configProps.setProperty("connectionTestQuery", connectionTestQuery);
+        configProps.setProperty("driverClassName", driverClassName);
+        configProps.setProperty("jdbcUrl", dataSourceUrl);
+
+        HikariConfig hc = new HikariConfig(configProps);
+        hc.setDataSourceProperties(dsProps);
+        return new HikariDataSource(hc);
+    }
+}
