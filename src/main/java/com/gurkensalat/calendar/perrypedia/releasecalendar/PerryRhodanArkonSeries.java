29bbe27aff62dca9dcf67854cb088d3f56a44385
Hakan Tandogan <hakan@tandogan.com>
2016-01-09 17:27:19 +0100

    Put Perry Rhodan Arkon on Thursday too

--- src/main/java/com/gurkensalat/calendar/perrypedia/releasecalendar/PerryRhodanArkonSeries.java
+++ src/main/java/com/gurkensalat/calendar/perrypedia/releasecalendar/PerryRhodanArkonSeries.java
@@ -4,8 +4,8 @@ import org.joda.time.DateTime;
 
 public class PerryRhodanArkonSeries extends AbstractSeries
 {
-    // PRAR 1 - 22.1.2016
-    private static final DateTime RELEASE_OF_ISSUE_1 = new DateTime().withDate(2016, 1, 22).withMillisOfDay(0);
+    // PRAR 1 - 22.1.2016 - one day less to put it on thursday
+    private static final DateTime RELEASE_OF_ISSUE_1 = new DateTime().withDate(2016, 1, 22).minusDays(1).withMillisOfDay(0);
 
     // PRAR 1 - Freitag, 22. Januar 2016
     private static DateTime releaseOfFirstIssue;
--- src/test/java/com/gurkensalat/calendar/perrypedia/releasecalendar/PerryRhodanArkonSeriesTest.java
+++ src/test/java/com/gurkensalat/calendar/perrypedia/releasecalendar/PerryRhodanArkonSeriesTest.java
@@ -27,9 +27,9 @@ public class PerryRhodanArkonSeriesTest
     public void testFirstIssueReleaseDate()
     {
         // PRAR 1
-        // Calculated: 2.1.2016
+        // Calculated: 21.1.2016
         // Perrypedia: Freitag, 22. Januar 2016
-        DateTime expected = new DateTime().withDate(2016, 1, 22).withMillisOfDay(0);
+        DateTime expected = new DateTime().withDate(2016, 1, 21).withMillisOfDay(0);
         DateTime actual = testable.getFirstIssueReleaseDate();
         assertEquals(expected, actual);
     }
@@ -38,9 +38,9 @@ public class PerryRhodanArkonSeriesTest
     public void testIssue2ReleaseDate()
     {
         // PRAR 2
-        // Calculated: 5. 2.2016
+        // Calculated: 4. 2.2016
         // Perrypedia: Freitag, 5. Februar 2016
-        DateTime expected = new DateTime().withDate(2016, 2, 5).withMillisOfDay(0);
+        DateTime expected = new DateTime().withDate(2016, 2, 4).withMillisOfDay(0);
         DateTime actual = testable.getIssueReleaseDate(2);
         assertEquals(expected, actual);
     }
@@ -49,9 +49,9 @@ public class PerryRhodanArkonSeriesTest
     public void testIssue12ReleaseDate()
     {
         // PRAR 12
-        // Calculated: 24. 6. 2016
+        // Calculated: 23. 6. 2016
         // Perrypedia: Unknown yet...
-        DateTime expected = new DateTime().withDate(2016, 6, 24).withMillisOfDay(0);
+        DateTime expected = new DateTime().withDate(2016, 6, 23).withMillisOfDay(0);
         DateTime actual = testable.getIssueReleaseDate(12);
         assertEquals(expected, actual);
     }
