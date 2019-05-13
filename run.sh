8c87427368f94fda9bb721060bb6b3d780dada13
Hakan Tandogan <hakan@tandogan.com>
2019-05-13 13:36:51 +0200

    [INFRA] Move log directories around

--- run.sh
+++ run.sh
@@ -7,36 +7,45 @@ VERBOSE=${1:-no}
 
 DEST=${HOME}/nextCloud-int/perrypedia-release-calendar
 
+LOGBASE=${HERE}/logs
+
+mkdir -p ${LOGBASE}
+
+LOGDIR=${LOGBASE}/$(date +%Y-%m)/$(date +%Y-%d)
+
+mkdir -p ${LOGDIR}
+
 cd ${HERE}
 
 git pull \
-    > ${HERE}/git.log \
-    2> ${HERE}/git.err
+    > ${LOGDIR}/git.log \
+    2> ${LOGDIR}/git.err
 
 mvn -U clean install \
-    > ${HERE}/mvn.log \
-    2> ${HERE}/mvn.err
+    > ${LOGDIR}/mvn.log \
+    2> ${LOGDIR}/mvn.err
 
 mkdir -p ${DEST}
 
 if [ -r ${DEST}/../on-nextCloud.txt ]
 then
     cd ${DEST}
-    java -jar ${HERE}/target/perrypedia-release-calendar-*-executable.jar \
+    java -DLOGBASE=${LOGBASE} \
 	 -Dsleep=30 \
-	 > ${HERE}/java-run.log \
-	 2> ${HERE}/java-run.err
+	 -jar ${HERE}/target/perrypedia-release-calendar-*-executable.jar \
+	 > ${LOGDIR}/java-run.log \
+	 2> ${LOGDIR}/java-run.err
 fi
 
 if [ ${VERBOSE} = "-v" ]
 then
     echo
 
-    cat ${HERE}/java-run.log
+    cat ${LOGDIR}/java-run.log
 
     echo
 
-    cat ${HERE}/java-run.err
+    cat ${LOGDIR}/java-run.err
 
     echo
 fi
